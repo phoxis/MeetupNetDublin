21608f22d303fc34889b4b71abb9dfc147b85019
Arjun Pakrashi <phoxis@gmail.com>
2018-10-07 03:55:30 +0100

    Code clean

--- results/MeetupNetDublinInteractive.py
+++ results/MeetupNetDublinInteractive.py
@@ -8,11 +8,6 @@ import time
 import os
 import threading
 
-#port = 8000;
-#Handler = http.server.SimpleHTTPRequestHandler;
-#httpd = socketserver.TCPServer(("", port), Handler);
-#print ("Port: ", port);
-
 target_file = {"mono": "meetup_plot_mono.gexf", "community": "meetup_plot_comm_colour.gexf"}
 option = "mono";
 
@@ -21,14 +16,6 @@ if len (sys.argv) > 1:
   if option not in target_file:
     option = "mono";
     print ("Invalid option \"{}\", Valid options: \"mono\", \"community\". Falling  back to default: \"mono\"", sys.argv[1]);
-
-#pid = os.fork ();
-#if pid == 0:
-  #webbrowser.open("http://0.0.0.0:8000/MeetupNetDublinInteractive/index.html#" + target_file[option], new = 1);
-#elif pid == -1:
-  #print ("Fork failed");
-#else:
-  #httpd.serve_forever();
 
 def start_server ():
   port = 8000;
@@ -40,8 +27,6 @@ def start_server ():
 t = threading.Thread (target = start_server);
 t.start();
 webbrowser.open("http://0.0.0.0:8000/MeetupNetDublinInteractive/index.html#" + target_file[option], new = 1);
-
-
 
 while True:
   try:
