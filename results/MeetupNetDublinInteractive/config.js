6b95cf318d4c3d3eac94ded3172758dc11013a0a
Arjun Pakrashi <phoxis@gmail.com>
2018-10-08 18:05:42 +0100

    Updating json files for faster loading

--- results/MeetupNetDublinInteractive/config.js
+++ results/MeetupNetDublinInteractive/config.js
@@ -1,7 +1,7 @@
 /*** USE THIS FILE TO SET OPTIONS ***/
 
 GexfJS.setParams({
-    graphFile : "meetup_plot_mono.gexf",
+    graphFile : "meetupNetDublinComm.json",
         /*
             The GEXF file to show ! -- can be overriden by adding
             a hash to the document location, e.g. index.html#celegans.gexf
